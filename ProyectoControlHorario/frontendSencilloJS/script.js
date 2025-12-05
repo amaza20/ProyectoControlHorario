131f6b055475c72e2edd1e504d5128203f7cc320
Angelo Maza <amazanew20@gmail.com>
2025-12-05 18:31:16 +0100

    Mejora https

--- ProyectoControlHorario/frontendSencilloJS/script.js
+++ ProyectoControlHorario/frontendSencilloJS/script.js
@@ -1,5 +1,5 @@
 // URL base de tu API
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://localhost:8080';
 
 // ============================================
 // FUNCIÓN: VERIFICAR SESIÓN
