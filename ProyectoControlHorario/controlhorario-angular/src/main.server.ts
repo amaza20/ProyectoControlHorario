8fd51688b5734629cbde77b4a11f4e7759b07633
Angelo Maza <amazanew20@gmail.com>
2025-12-17 18:01:24 +0100

    Implementando Angular en el FrontEnd 3.0

--- ProyectoControlHorario/controlhorario-angular/src/main.server.ts
+++ ProyectoControlHorario/controlhorario-angular/src/main.server.ts
@@ -1,7 +1,11 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app';
 
-const bootstrap = () => bootstrapApplication(AppComponent, appConfig);
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering()
+  ]
+};
 
-export default bootstrap;
+export const config = mergeApplicationConfig(appConfig, serverConfig);
